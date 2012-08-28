4ae97928f36a6382fe52a72b33b0c6d576119694
kefa <jilong.liao@gmail.com>
2012-08-28 13:52:33 -0400

    new application architecture

--- runserver.py
+++ runserver.py
@@ -1,9 +1,12 @@
 from tornado.wsgi import WSGIContainer
 from tornado.ioloop import IOLoop
-from tornado.web import FallbackHandler, RequestHandler, Application
+from tornado.web import FallbackHandler
+from tornado.web import RequestHandler
+from tornado.web import Application
 from gethem import app
 
 tr = WSGIContainer(app)
+print app
 
 application = Application([
   (r".*", FallbackHandler, dict(fallback=tr)),
