2d3dcdea24cde96e6f7d00a0aece2781e4f1a422
Jilong Liao <jliao2@utk.edu>
2012-10-27 22:12:20 -0400

    commit db file

--- db/schema.sql
+++ db/schema.sql
@@ -1,60 +1,127 @@
-drop database if exists gethemdb;
-create database gethemdb;
-use gethemdb;
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1deb1
+-- http://www.phpmyadmin.net
+--
+-- 主机: localhost
+-- 生成日期: 2012 年 10 月 27 日 22:11
+-- 服务器版本: 5.5.24
+-- PHP 版本: 5.3.10-1ubuntu3.4
 
-drop table if exists user;
-create table user (
-  user_id integer primary key AUTO_INCREMENT,
-  username varchar(80) not null,
-  email varchar(100) not null,
-  pw_hash varchar(100) not null
-);
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-drop table if exists follower;
-create table follower (
-  who_id integer,
-  whom_id integer
-);
 
-drop table if exists need;
-create table need (
-  need_id integer primary key auto_increment,
-  need_author_id integer not null,
-  need_title varchar(400) not null,
-  need_content varchar(800) not null,
-  need_pub_date integer
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
-drop table if exists provide;
-create table provide (
-  provide_id integer primary key auto_increment,
-  provide_author_id integer not null,
-  provide_title varchar(400) not null,
-  provide_content varchar(800) not null,
-  provide_pub_date integer
-);
+--
+-- 数据库: `gethemdb`
+--
+CREATE DATABASE `gethemdb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `gethemdb`;
 
-drop table if exists matchpost;
-create table matchpost (
-  need_id integer,
-  provide_id integer,
-  score float(53,16)
-);
+-- --------------------------------------------------------
 
-drop table if exists needpic;
-create table needpic (
-  needpic_id integer primary key auto_increment,
-  needpic_url varchar(400) not null,
-  needpic_user_id integer,
-  need_id integer,
-  needpic_pub_date integer
-);
+--
+-- 表的结构 `follower`
+--
 
-drop table if exists providepic;
-create table providepic (
-  providepic_id integer primary key auto_increment,
-  providepic_url varchar(400) not null,
-  provdepic_user_id integer,
-  provide_id integer,
-  providepic_pub_date integer
-);
+DROP TABLE IF EXISTS `follower`;
+CREATE TABLE IF NOT EXISTS `follower` (
+  `who_id` int(11) DEFAULT NULL,
+  `whom_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `matchpost`
+--
+
+DROP TABLE IF EXISTS `matchpost`;
+CREATE TABLE IF NOT EXISTS `matchpost` (
+  `need_id` int(11) DEFAULT NULL,
+  `provide_id` int(11) DEFAULT NULL,
+  `score` float(53,16) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `need`
+--
+
+DROP TABLE IF EXISTS `need`;
+CREATE TABLE IF NOT EXISTS `need` (
+  `need_id` int(11) NOT NULL AUTO_INCREMENT,
+  `need_author_id` int(11) NOT NULL,
+  `need_title` varchar(400) NOT NULL,
+  `need_content` varchar(800) NOT NULL,
+  `need_pub_date` int(11) DEFAULT NULL,
+  PRIMARY KEY (`need_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `need_img`
+--
+
+DROP TABLE IF EXISTS `need_img`;
+CREATE TABLE IF NOT EXISTS `need_img` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `need_post_id` int(11) NOT NULL,
+  `uri` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `provide`
+--
+
+DROP TABLE IF EXISTS `provide`;
+CREATE TABLE IF NOT EXISTS `provide` (
+  `provide_id` int(11) NOT NULL AUTO_INCREMENT,
+  `provide_author_id` int(11) NOT NULL,
+  `provide_title` varchar(400) NOT NULL,
+  `provide_content` varchar(800) NOT NULL,
+  `provide_pub_date` int(11) DEFAULT NULL,
+  PRIMARY KEY (`provide_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=18 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `provide_img`
+--
+
+DROP TABLE IF EXISTS `provide_img`;
+CREATE TABLE IF NOT EXISTS `provide_img` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `provide_post_id` int(11) NOT NULL,
+  `uri` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(80) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `pw_hash` varchar(100) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
